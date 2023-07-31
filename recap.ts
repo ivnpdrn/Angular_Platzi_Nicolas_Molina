0b27df0a26b6dda78a80b2c4b5e79bc48d08bb56
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-07-31 18:09:18 -0400

    Conceptos basicos de TypeScript para usar Angular

--- recap.ts
+++ recap.ts
@@ -5,14 +5,18 @@ const sum = (a: number, b:number) => {
 sum(1,2);
 
 class Person {
-  age: number;
-  lastName: string;
 
-  constructor(age: number, lastName: string) {
-    this.age = age;
-    this.lastName = lastName;
+  // age: number;
+  // lastName: string;
+
+  //constructor(age: number, lastName: string) {
+  //  this.age = age;
+  //  this.lastName = lastName;
+
+  constructor(public age: number, public lastName: string) {}
+
   }
-}
+
 
 const nico = new Person(15, 'Molina');
 nico.age;
